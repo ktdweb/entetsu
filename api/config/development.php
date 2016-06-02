ce506d5e8700c8a54a5fb6dad416edef08526dfb
Keiji Kobayashi <keiji@ktd-web.com>
2016-06-03 04:38:48 +0900

    first feature

---
+++ api/config/development.php
@@ -0,0 +1,29 @@
+<?php
+const ENVIRONMENT_MODE = 'development';
+const DEBUG_DEFAULT    = true;
+
+// can use array as const in php5.6 or upper
+const BASIC_AUTH_USER  = 'assist';
+const BASIC_AUTH_PASS  = 'assist012';
+
+/*
+ * Database
+ */
+const DB_HOST       = '0.0.0.0';
+const DB_NAME       = 'ad110xb4ir_assist';
+const DB_USER       = 'ad110xb4ir';
+const DB_PASS       = 'fpMNe8Dx';
+const DB_PORT       = '3306';
+const DB_CHARSET    = 'utf8';
+
+/*
+ * Mailer
+ */
+const MAIL_HOST     = '127.0.0.1';
+const MAIL_PORT     = '25';
+const MAIL_USER     = 'system@entetsu-assist.co.jp';
+const MAIL_PASS     = 'WooTeit4';
+
+const MAIL_FROM     = 'info@example.com';
+const MAIL_NAME     = 'System Notification';
+const MAIL_ADDR     = 'admin@example.com';
