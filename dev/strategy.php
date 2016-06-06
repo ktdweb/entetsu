4046fd30c21fda34692a95a003e13aceea89ea42
Keiji Kobayashi <keiji@ktd-web.com>
2016-06-06 19:51:26 +0900

    仮ページをさらに追加

---
+++ dev/strategy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/style.css" media="all">
+</head>
+<body>
+    <article>
+        <?php include 'includes/header.php' ?>
+
+        <img src="imgs/top_top.jpg" width="1040" />
+
+        <h1>遠鉄アシストの戦略</h1>
+        <h2>仮ページ</h2>
+
+        <?php include 'includes/footer.php' ?>
+    </article>
+</body>
+</html>
