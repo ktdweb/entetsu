c68b482cd2e248e34e599f9940d9ca5f0d3eedd9
Keiji Kobayashi <keiji@ktd-web.com>
2016-07-19 06:03:51 +0900

    トップのカラム

--- dist/build.js
+++ dist/build.js
@@ -867,7 +867,77 @@ function _inherits(subClass, superClass) {
   }subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
 }
 
-var page;
+var page, data;
+
+var contents = {
+  cleaning: {
+    h1: '清掃でお困りの方',
+    column: [{
+      title: 'サービス案内',
+      desc: '遠鉄アシストならではのきめ細かいサービスと、まかせて安心のトータルサポートでお応えします。'
+    }, {
+      title: '遠鉄アシストの清掃業務管理',
+      desc: '遠鉄アシストの組織力が、清掃業務を円滑にバックアップします。'
+    }, {
+      title: '遠鉄アシストの特徴',
+      desc: '遠鉄アシストの組織力が、清掃業務を円滑にバックアップします。'
+    }, {
+      title: '遠鉄アシストのサポート',
+      desc: '専門の資格を持ったスタッフが、迅速に対応。安心してご利用頂けます。'
+    }]
+  },
+
+  building: {
+    h1: 'ビル管理でお困りの方',
+    column: [{
+      title: 'サービス案内',
+      desc: '遠鉄アシストならではのきめ細かいサービスと、まかせて安心のトータルサポートでお応えします。'
+    }, {
+      title: '遠鉄アシストの取り組み',
+      desc: '地域を幅広くサポートする遠鉄グループだからできること。ビルが担う役割を最大限まで高めます。遠鉄アシストの組織力が、清掃業務を円滑にバックアップします。'
+    }, {
+      title: '遠鉄アシストの特徴',
+      desc: '多種多様な有資格者による高度な技術力と、常にエンドユーザーの視点に立ったきめ細かいサービスで、お客様の満足度アップに貢献します。'
+    }, {
+      title: '遠鉄アシストのサポート',
+      desc: '専門の資格を持ったスタッフが、迅速に対応。安心してご利用頂けます。'
+    }]
+  },
+
+  mansion: {
+    h1: 'マンション管理でお困りの方',
+    column: [{
+      title: 'サービス案内',
+      desc: '遠鉄アシストならではのきめ細かいサービスと、まかせて安心のトータルサポートでお応えします。'
+    }, {
+      title: '遠鉄アシストの取り組み',
+      desc: '大切な資産を守り、入居者の皆様の期待に応えるために、遠鉄アシストでは安心への取り組みに力を注いでいます。信頼の業務で皆様の豊かな暮らしに貢献します。'
+    }, {
+      title: '遠鉄アシストの特徴',
+      desc: 'マンションライフを熟知した遠鉄グループの総合力を活かし、入居者の皆様の立場に立ってきめ細かくサポート。ハードとソフトの両面から管理業務を担います。'
+    }, {
+      title: '遠鉄アシストのサポート',
+      desc: '専門の資格を持ったスタッフが、迅速に対応。安心してご利用頂けます。'
+    }]
+  },
+
+  driving: {
+    h1: '運転・送迎でお困りの方',
+    column: [{
+      title: 'サービス案内',
+      desc: '遠鉄アシストならではのきめ細かいサービスと、まかせて安心のトータルサポートでお応えします。'
+    }, {
+      title: '導入事例',
+      desc: '運行管理事業の導入事例です。お客様のニーズに合わせて、臨機応変に問題を解決いたします。'
+    }, {
+      title: '遠鉄アシストの特徴',
+      desc: '遠鉄グループ輸送部門の実績とノウハウ、運転士の確かな安全技術、笑顔の接客・接遇で、多くのお客様から信頼をいただいております。'
+    }, {
+      title: '安全・安心・快適への取り組み',
+      desc: 'お客様の安全・安心・快適をモットーに、遠鉄アシストでは 運転士の安全教育を徹底。経験と実績に甘んじることなく、技術と知識の強化を図っています。'
+    }]
+  }
+};
 
 var Article = function (_React$Component) {
   _inherits(Article, _React$Component);
@@ -878,9 +948,7 @@ var Article = function (_React$Component) {
     var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Article).call(this, props));
 
     _this.state = {
-      page: props.route.page,
-      width: 1040,
-      height: 375
+      page: props.route.page
     };
     return _this;
   }
@@ -892,12 +960,12 @@ var Article = function (_React$Component) {
     }
   }, {
     key: 'componentDidMount',
-    value: function componentDidMount() {
+    value: function componentDidMount() {}
+  }, {
+    key: 'componentWillMount',
+    value: function componentWillMount() {
       this.updateState();
     }
-  }, {
-    key: 'componentWillUnmount',
-    value: function componentWillUnmount() {}
   }, {
     key: 'updateState',
     value: function updateState() {
@@ -909,20 +977,29 @@ var Article = function (_React$Component) {
         page = this.props.route.page;
       }
       var src = 'imgs/pages/' + page + '.jpg';
+
       this.setState({
         src: src,
         page: page
       });
+
+      data = contents[page];
+    }
+  }, {
+    key: 'toId',
+    value: function toId(str) {
+      var first = str.charAt(0).toUpperCase();
+      return first + str.slice(1);
     }
   }, {
     key: 'render',
     value: function render() {
-      return _react2.default.createElement('article', { id: 'Building' }, _react2.default.createElement(_reactDocumentTitle2.default, { title: '遠鉄アシスト | ビル管理' }), _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, _react2.default.createElement('img', {
-        src: this.state.src,
-        width: this.state.width,
-        height: this.state.height,
-        alt: this.state.page
-      })));
+      return _react2.default.createElement('article', {
+        id: this.toId(page),
+        className: 'pf-Top'
+      }, _react2.default.createElement(_reactDocumentTitle2.default, {
+        title: data.h1 + ' | 遠鉄アシスト'
+      }), _react2.default.createElement('nav', null, _react2.default.createElement('ul', null, _react2.default.createElement('li', null, _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, data.h1 + 'トップ')), _react2.default.createElement('li', null, _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, data.column[0].title)), _react2.default.createElement('li', null, _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, data.column[1].title)), _react2.default.createElement('li', null, _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, data.column[2].title)), _react2.default.createElement('li', null, _react2.default.createElement(_reactRouter.Link, { to: this.state.page + '_detail' }, data.column[3].title)))), _react2.default.createElement('section', null, _react2.default.createElement('div', null, _react2.default.createElement('h2', null, data.column[0].title), _react2.default.createElement('p', null, data.column[0].desc)), _react2.default.createElement('div', null, _react2.default.createElement('h2', null, data.column[1].title), _react2.default.createElement('p', null, data.column[1].desc)), _react2.default.createElement('div', null, _react2.default.createElement('h2', null, data.column[2].title), _react2.default.createElement('p', null, data.column[2].desc)), _react2.default.createElement('div', null, _react2.default.createElement('h2', null, data.column[3].title), _react2.default.createElement('p', null, data.column[3].desc))));
     }
   }]);
 
