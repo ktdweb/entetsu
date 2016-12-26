6c4289f78a7cb3466ef563d567f68e617b4019ff
Keiji Kobayashi <keiji@ktd-web.com>
2016-12-26 18:41:32 +0900

    mobileを追加

---
+++ mobile/w02.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=320px, user-scalable=no, initial-scale=1, maximum-scale=1" />
+<title>遠鉄アシスト｜仕事を探す</title>
+<?php include 'include/meta.inc'; ?>
+
+</head>
+<body>
+<div id="view">
+<div class="wrap">
+<?php include 'include/header.inc'; ?>
+
+<div id="contents">
+<article>
+<h2 class="stitle"><img src="imgs/title_works.png" alt="検索結果" width="100%"></h2>
+<section>
+<div class="worksDetail">
+    <div class="workData">
+        <div class="workName">クリーンスタッフ</div>
+        <ul class="workPoint">
+        	<li>時給: 860円</li>
+        	<li>17:00～19:30 </li>
+        </ul>
+        <dl class="workInfo">
+        	<dt>仕事の内容</dt>
+            <dd>温浴施設内の日常清掃（脱衣所・洗い場・階段等）</dd>
+            
+            <dt>勤務地</dt>
+            <dd>温浴施設（東区天王町）</dd>
+            
+            <dt>時間</dt>
+            <dd>5:30~8:30</dd>
+            
+            <dt>休憩時間</dt>
+            <dd></dd>
+            
+            <dt>給与</dt>
+            <dd>時給940~950円　※見習期間3ヶ月は、時給-10円</dd>
+            
+            <dt>日数</dt>
+            <dd>週3~4日（シフト制）</dd>
+            
+            <dt>休日</dt>
+            <dd></dd>
+            
+            <dt>雇用形態</dt>
+            <dd>パート</dd>
+            
+            <dt>雇用期間</dt>
+            <dd></dd>
+            
+            <dt>学歴</dt>
+            <dd></dd>
+            
+            <dt>職場の雰囲気</dt>
+            <dd>女性活躍中　未経験者歓迎　丁寧な研修あり♪</dd>
+            
+            <dt>資格</dt>
+            <dd></dd>
+            
+            <dt>備考</dt>
+            <dd>交通費規定支給、制服貸与、車通勤可</dd>
+        </dl>
+        
+        <div class="worksBtnBox">
+        	<a class="btnBack" href="top.php">戻る</a>
+        </div>
+    </div>
+</div>
+</section>
+
+</article>
+</div><!-- /.contents -->
+
+<?php include 'include/footer.inc'; ?>
+
+</div><!-- /.wrap -->
+</div><!-- /#view -->
+
+<?php include 'include/js.inc'; ?>
+
+
+</body>
+</html>
---
+++ mobile/w06.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=320px, user-scalable=no, initial-scale=1, maximum-scale=1" />
+<title>遠鉄アシスト｜仕事を探す</title>
+<?php include 'include/meta.inc'; ?>
+
+</head>
+<body>
+<div id="view">
+<div class="wrap">
+<?php include 'include/header.inc'; ?>
+
+<div id="contents">
+<article>
+<h2 class="stitle"><img src="imgs/title_works.png" alt="検索結果" width="100%"></h2>
+<section>
+<div class="worksDetail">
+    <div class="workData">
+        <div class="workName">駐車場管理業務（浜松市駅南駐車場）</div>
+        <ul class="workPoint">
+        	<li>時給: 800円</li>
+        	<li>5:45~20:00（シフト制）　例：5:45~15:00、10:00~17:30、11:00~19:00など</li>
+        </ul>
+        <dl class="workInfo">
+        	<dt>仕事の内容</dt>
+            <dd>街中の駐車場で、売上金やサービス券の回収・集計、駐車場機械操作、車両誘導などをするお仕事です。未経験者歓迎！</dd>
+            
+            <dt>勤務地</dt>
+            <dd>浜松市街中</dd>
+            
+            <dt>時間</dt>
+            <dd>5:45~20:00（シフト制）　例：5:45~15:00、10:00~17:30、11:00~19:00など</dd>
+            
+            <dt>休憩時間</dt>
+            <dd>60分</dd>
+            
+            <dt>給与</dt>
+            <dd>時給: 800円</dd>
+            
+            <dt>日数</dt>
+            <dd>週3日程度</dd>
+            
+            <dt>休日</dt>
+            <dd>シフト制</dd>
+            
+            <dt>雇用形態</dt>
+            <dd>アルバイト</dd>
+            
+            <dt>雇用期間</dt>
+            <dd>長期または1ヵ月程度の短期もOK</dd>
+            
+            <dt>学歴</dt>
+            <dd>不問</dd>
+            
+            <dt>職場の雰囲気</dt>
+            <dd>60代の男性多数活躍中！</dd>
+            
+            <dt>資格</dt>
+            <dd>普通運転免許</dd>
+            
+            <dt>備考</dt>
+            <dd>交通費規定支給、車通勤可、制服貸与、研修有り</dd>
+        </dl>
+        
+        <div class="worksBtnBox">
+        	<a class="btnBack" href="top.php">戻る</a>
+        </div>
+    </div>
+</div>
+</section>
+
+</article>
+</div><!-- /.contents -->
+
+<?php include 'include/footer.inc'; ?>
+
+</div><!-- /.wrap -->
+</div><!-- /#view -->
+
+<?php include 'include/js.inc'; ?>
+
+
+</body>
+</html>
